52fc79a39498d87d94c3cf5aee8dcf8d3051773b
noor <noor@10.0.0.128>
2017-05-03 20:32:52 -0500

    commit

--- javainterviews/src/main/java/com/noor/binarysearchtree/App.java
+++ javainterviews/src/main/java/com/noor/binarysearchtree/App.java
@@ -8,5 +8,10 @@ public class App {
 		b.addNode(10);
 		b.addNode(20);
 		b.addNode(8);
+		b.addNode(23);
+		b.addNode(21);
+		b.addNode(17);
+		b.addNode(71);
+		b.addNode(6);
 	}
 }
--- javainterviews/src/main/java/com/noor/binarysearchtree/BinaryTree.java
+++ javainterviews/src/main/java/com/noor/binarysearchtree/BinaryTree.java
@@ -4,24 +4,35 @@ public class BinaryTree {
 
 	private Node root;
 	
-	
-	
 	public void addNode(int data) {
-		
-		Node node = new Node(data);
-		if(root == null) {
-			root = node;
-		}else{
-			Node focusNode = root;
-			Node n = new Node(data);
-			
-			if(data<=focusNode.getValue()) {
-				 root.setLeftChild(n);
-			}else{
-				root.setRightChild(n);
-			}
-			focusNode = n;
+		Node n = new Node(data);
+		if(root==null) {
+			System.out.print("\t\t\t\t"+n.getValue());
 		}
+		System.out.println("");
+		root = insertNode(root,n);
+	}
+	
+	public Node insertNode(Node head, Node node) {
+		
+		if(head==null) {
+			
+			return node;
+		}
+		
+		if(node.getValue()<=head.getValue()) {
+			Node n = insertNode(head.getLeftChild(),node);
+			head.setLeftChild(n);
+			System.out.print("\t\t\t"+head.getLeftChild().getValue());
+		}else{
+			Node n = insertNode(head.getRightChild(),node);
+			head.setRightChild(n);
+			System.out.print("\t\t\t\t\t\t"+head.getRightChild().getValue());
+		}
+		
+		return head;
+		
+		
 	}
 	
 	
